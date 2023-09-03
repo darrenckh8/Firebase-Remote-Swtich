ae9a4e4a84458bf5919cf87c42cfd8b2fcdf9196
Darren <darrenckh8@gmail.com>
2023-09-03 22:04:31 +0800

    Formatted

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 #if defined(ESP32)
-  #include <WiFi.h>
+#include <WiFi.h>
 #elif defined(ESP8266)
-  #include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #endif
 #include <Firebase_ESP_Client.h>
 
@@ -34,10 +34,12 @@ FirebaseConfig config;
 String listenerPath = "board1/outputs/digital/";
 
 // Initialize WiFi
-void initWiFi() {
+void initWiFi()
+{
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi ..");
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     Serial.print('.');
     delay(1000);
   }
@@ -46,20 +48,22 @@ void initWiFi() {
 }
 
 // Callback function that runs on database changes
-void streamCallback(FirebaseStream data){
+void streamCallback(FirebaseStream data)
+{
   Serial.printf("stream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\n\n",
                 data.streamPath().c_str(),
                 data.dataPath().c_str(),
                 data.dataType().c_str(),
                 data.eventType().c_str());
-  printResult(data); //see addons/RTDBHelper.h
+  printResult(data); // see addons/RTDBHelper.h
   Serial.println();
 
   // Get the path that triggered the function
   String streamPath = String(data.dataPath());
 
   // if the data returned is an integer, there was a change on the GPIO state on the following path /{gpio_number}
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer){
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_integer)
+  {
     String gpio = streamPath.substring(1);
     int state = data.intData();
     Serial.print("GPIO: ");
@@ -71,55 +75,60 @@ void streamCallback(FirebaseStream data){
 
   /* When it first runs, it is triggered on the root (/) path and returns a JSON with all keys
   and values of that path. So, we can get all values from the database and updated the GPIO states*/
-  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json){
+  if (data.dataTypeEnum() == fb_esp_rtdb_data_type_json)
+  {
     FirebaseJson json = data.to<FirebaseJson>();
 
     // To iterate all values in Json object
     size_t count = json.iteratorBegin();
     Serial.println("\n---------");
-    for (size_t i = 0; i < count; i++){
-        FirebaseJson::IteratorValue value = json.valueAt(i);
-        int gpio = value.key.toInt();
-        int state = value.value.toInt();
-        Serial.print("STATE: ");
-        Serial.println(state);
-        Serial.print("GPIO:");
-        Serial.println(gpio);
-        digitalWrite(gpio, state);
-        Serial.printf("Name: %s, Value: %s, Type: %s\n", value.key.c_str(), value.value.c_str(), value.type == FirebaseJson::JSON_OBJECT ? "object" : "array");
+    for (size_t i = 0; i < count; i++)
+    {
+      FirebaseJson::IteratorValue value = json.valueAt(i);
+      int gpio = value.key.toInt();
+      int state = value.value.toInt();
+      Serial.print("STATE: ");
+      Serial.println(state);
+      Serial.print("GPIO:");
+      Serial.println(gpio);
+      digitalWrite(gpio, state);
+      Serial.printf("Name: %s, Value: %s, Type: %s\n", value.key.c_str(), value.value.c_str(), value.type == FirebaseJson::JSON_OBJECT ? "object" : "array");
     }
     Serial.println();
     json.iteratorEnd(); // required for free the used memory in iteration (node data collection)
   }
-  
-  //This is the size of stream payload received (current and max value)
-  //Max payload size is the payload size under the stream path since the stream connected
-  //and read once and will not update until stream reconnection takes place.
-  //This max value will be zero as no payload received in case of ESP8266 which
-  //BearSSL reserved Rx buffer size is less than the actual stream payload.
+
+  // This is the size of stream payload received (current and max value)
+  // Max payload size is the payload size under the stream path since the stream connected
+  // and read once and will not update until stream reconnection takes place.
+  // This max value will be zero as no payload received in case of ESP8266 which
+  // BearSSL reserved Rx buffer size is less than the actual stream payload.
   Serial.printf("Received stream payload size: %d (Max. %d)\n\n", data.payloadLength(), data.maxPayloadLength());
 }
 
-void streamTimeoutCallback(bool timeout){
+void streamTimeoutCallback(bool timeout)
+{
   if (timeout)
     Serial.println("stream timeout, resuming...\n");
   if (!stream.httpConnected())
     Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
 }
 
-void setup(){
+void setup()
+{
   Serial.begin(115200);
   initWiFi();
 
   // Initialize Outputs
- int outputPins[] = {2, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33};
+  int outputPins[] = {2, 4, 5, 12, 13, 14, 15, 16, 17, 18, 19, 21, 22, 23, 25, 26, 27, 32, 33};
 
   // Iterate through the pins and set them as outputs
-  for (int i = 0; i < sizeof(outputPins) / sizeof(outputPins[0]); i++) {
-    pinMode(outputPins[i], OUTPUT); // Set the current pin as an output
+  for (int i = 0; i < sizeof(outputPins) / sizeof(outputPins[0]); i++)
+  {
+    pinMode(outputPins[i], OUTPUT);   // Set the current pin as an output
     digitalWrite(outputPins[i], LOW); // Set the initial state to LOW
   }
-  
+
   // Assign the api key (required)
   config.api_key = API_KEY;
 
@@ -133,7 +142,7 @@ void setup(){
   Firebase.reconnectWiFi(true);
 
   // Assign the callback function for the long running token generation task */
-  config.token_status_callback = tokenStatusCallback; //see addons/TokenHelper.h
+  config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
 
   // Assign the maximum retry of token generation
   config.max_token_generation_retry = 5;
@@ -152,8 +161,10 @@ void setup(){
   delay(2000);
 }
 
-void loop(){
-  if (Firebase.isTokenExpired()){
+void loop()
+{
+  if (Firebase.isTokenExpired())
+  {
     Firebase.refreshToken(&config);
     Serial.println("Refresh token");
   }
