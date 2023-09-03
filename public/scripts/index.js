b851b3de7637631ec3abe85c1d2470f5eb9b3eb1
Darren <darrenckh8@gmail.com>
2023-09-03 20:44:12 +0800

    Added multi-board support

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,65 +1,88 @@
-const loginElement = document.querySelector('#login-form');
+const loginElement = document.querySelector("#login-form");
 const contentElement = document.querySelector("#content-sign-in");
-const userDetailsElement = document.querySelector('#user-details');
+const userDetailsElement = document.querySelector("#user-details");
 const authBarElement = document.querySelector("#authentication-bar");
 
-// Elements for GPIO states
-const stateElements = {}; // Store state elements in an object
+// Create an object to store GPIO pins, states, and buttons for each board
+const boards = {};
 
-// Button Elements
-const btnOn = {};
-const btnOff = {};
+// Function to set up GPIO pins for a specific board
+const setupBoard = (boardName) => {
+  boards[boardName] = {};
 
-// Database references for GPIO pins
-const dbPathOutputs = {}; // Store database paths in an object
-const dbRefOutputs = {}; // Store database references in an object
+  // Elements for GPIO states
+  boards[boardName].stateElements = {}; // Store state elements in an object
 
-// Initialize GPIO pins dynamically
-for (let gpioPin = 2; gpioPin <= 33; gpioPin++) {
-  // Create state element
-  stateElements[gpioPin] = document.getElementById(`state${gpioPin}`);
+  // Button Elements
+  boards[boardName].btnOn = {};
+  boards[boardName].btnOff = {};
 
-  // Create button elements
-  btnOn[gpioPin] = document.getElementById(`btn${gpioPin}On`);
-  btnOff[gpioPin] = document.getElementById(`btn${gpioPin}Off`);
+  // Database references for GPIO pins
+  boards[boardName].dbPathOutputs = {}; // Store database paths in an object
+  boards[boardName].dbRefOutputs = {}; // Store database references in an object
 
-  // Define database path for each GPIO pin
-  dbPathOutputs[gpioPin] = `board1/outputs/digital/${gpioPin}`;
-  dbRefOutputs[gpioPin] = firebase.database().ref().child(dbPathOutputs[gpioPin]);
+  for (let gpioPin = 2; gpioPin <= 33; gpioPin++) {
+    // Create state element
+    boards[boardName].stateElements[gpioPin] = document.getElementById(
+      `state${boardName}-${gpioPin}`
+    );
 
-  // Update states depending on the database value
-  dbRefOutputs[gpioPin].on('value', snap => {
-    if (snap.val() == 1) {
-      stateElements[gpioPin].innerText = "ON";
-    } else {
-      stateElements[gpioPin].innerText = "OFF";
-    }
-  });
+    // Create button elements
+    boards[boardName].btnOn[gpioPin] = document.getElementById(
+      `btn${boardName}-${gpioPin}On`
+    );
+    boards[boardName].btnOff[gpioPin] = document.getElementById(
+      `btn${boardName}-${gpioPin}Off`
+    );
 
-  // Update database upon button click
-  btnOn[gpioPin].onclick = () => {
-    dbRefOutputs[gpioPin].set(1);
-  };
+    // Define database path for each GPIO pin
+    boards[boardName].dbPathOutputs[
+      gpioPin
+    ] = `${boardName}/outputs/digital/${gpioPin}`;
+    boards[boardName].dbRefOutputs[gpioPin] = firebase
+      .database()
+      .ref()
+      .child(boards[boardName].dbPathOutputs[gpioPin]);
 
-  btnOff[gpioPin].onclick = () => {
-    dbRefOutputs[gpioPin].set(0);
-  };
-}
+    // Update states depending on the database value
+    boards[boardName].dbRefOutputs[gpioPin].on("value", (snap) => {
+      if (snap.val() == 1) {
+        boards[boardName].stateElements[gpioPin].innerText = "ON";
+      } else {
+        boards[boardName].stateElements[gpioPin].innerText = "OFF";
+      }
+    });
+
+    // Update database upon button click
+    boards[boardName].btnOn[gpioPin].onclick = () => {
+      boards[boardName].dbRefOutputs[gpioPin].set(1);
+    };
+
+    boards[boardName].btnOff[gpioPin].onclick = () => {
+      boards[boardName].dbRefOutputs[gpioPin].set(0);
+    };
+  }
+};
+
+// Initialize GPIO pins for each board
+boardNames.forEach((boardName) => {
+  setupBoard(boardName);
+});
 
 // MANAGE LOGIN/LOGOUT UI
 const setupUI = (user) => {
   if (user) {
     // Toggle UI elements
-    loginElement.style.display = 'none';
-    contentElement.style.display = 'block';
-    authBarElement.style.display ='block';
-    userDetailsElement.style.display ='block';
+    loginElement.style.display = "none";
+    contentElement.style.display = "block";
+    authBarElement.style.display = "block";
+    userDetailsElement.style.display = "block";
     userDetailsElement.innerHTML = user.email;
   } else {
     // Toggle UI elements
-    loginElement.style.display = 'block';
-    authBarElement.style.display ='none';
-    userDetailsElement.style.display ='none';
-    contentElement.style.display = 'none';
+    loginElement.style.display = "block";
+    authBarElement.style.display = "none";
+    userDetailsElement.style.display = "none";
+    contentElement.style.display = "none";
   }
-}
+};
